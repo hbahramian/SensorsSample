c02611f5656179a2a44ad189c168441a572203d9
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-10-31 23:56:52 -0400

    .

--- app/src/main/java/edu/iu/habahram/sensorssample/Sensors.kt
+++ app/src/main/java/edu/iu/habahram/sensorssample/Sensors.kt
@@ -19,3 +19,11 @@ class ProximitySensor(
     sensorFeature = PackageManager.FEATURE_SENSOR_PROXIMITY,
     sensorType = Sensor.TYPE_PROXIMITY
 )
+
+class AccelerometerSensor(
+    context: Context
+): AndroidSensor(
+    context = context,
+    sensorFeature = PackageManager.FEATURE_SENSOR_ACCELEROMETER,
+    sensorType = Sensor.TYPE_ACCELEROMETER
+)
